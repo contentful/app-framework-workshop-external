dc9cc2cc266abd9f34d867ba446a4eb1b3ac6518
Andi Pätzold <andreas.paetzold@contentful.com>
2024-04-04 15:10:13 +0200

    Field/Sidebar: useAutoResizer

--- src/locations/Sidebar.tsx
+++ src/locations/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { Paragraph } from "@contentful/f36-components";
-import { useFieldValue } from "@contentful/react-apps-toolkit";
+import { useAutoResizer, useFieldValue } from "@contentful/react-apps-toolkit";
 
 const FIELD_ID = "text";
 
 const Sidebar = () => {
+  useAutoResizer();
   const [text] = useFieldValue<string | undefined>(FIELD_ID);
 
   return <Paragraph>{(text ?? "").length} chars</Paragraph>;
