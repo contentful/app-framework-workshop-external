69738e2bae2d6fa81046dfcdd0d0c160671daeba
Andi Pätzold <andreas.paetzold@contentful.com>
2024-04-04 15:08:42 +0200

    Sidebar: Using React Apps Toolkit

--- src/locations/Sidebar.tsx
+++ src/locations/Sidebar.tsx
@@ -1,23 +1,10 @@
-import { SidebarAppSDK } from "@contentful/app-sdk";
 import { Paragraph } from "@contentful/f36-components";
-import { useSDK } from "@contentful/react-apps-toolkit";
-import { useMemo, useState } from "react";
+import { useFieldValue } from "@contentful/react-apps-toolkit";
 
 const FIELD_ID = "text";
 
 const Sidebar = () => {
-  const sdk = useSDK<SidebarAppSDK>();
-  const [text, setText] = useState<string | undefined>(
-    sdk.entry.fields[FIELD_ID].getValue()
-  );
-
-  useMemo(() => {
-    const unsubscribe = sdk.entry.fields[FIELD_ID].onValueChanged((value) => {
-      setText(value);
-    });
-
-    return unsubscribe;
-  }, []);
+  const [text] = useFieldValue<string | undefined>(FIELD_ID);
 
   return <Paragraph>{(text ?? "").length} chars</Paragraph>;
 };
